1bfff6026cdc1d69b101b394b5152a4badfeb5b0
Tobias-Schulz-AI <schulz_tobias_sp@icloud.com>
2021-03-24 16:20:04 +0100

    updated annotations

--- face_detector_ssd/face_detector_src/annotations.py
+++ face_detector_ssd/face_detector_src/annotations.py
@@ -3,19 +3,22 @@ import numpy as np
         
 def annotate_heads(frame, face_boxes, confidence):
     frame_copy = frame.copy()
-    for i in range(0, len(confidence)):
-        (startX, startY, endX, endY) = face_boxes[i]
-        if (np.array([startX, startY, endX, endY])>0).all():
-            print('Coordinates in annotate heads:', (startX, startY, endX, endY))
-            text = "{:.2f}%".format(confidence[i] * 100)
-            y = endY + 20 if endY + 20 > 10 else endY - 10
-            cv2.rectangle(frame, (startX, startY), (endX, endY),
-                            (0, 0, 255), 2)
-            cv2.putText(frame, text, (startX, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 1)
-            return frame
-        else:
-            return frame_copy
+    if face_boxes is not None:
+        for i in range(0, len(confidence)):
+            (startX, startY, endX, endY) = face_boxes[i]
+            if (np.array([startX, startY, endX, endY])>0).all():
+                print('Coordinates in annotate heads:', (startX, startY, endX, endY))
+                text = "{:.2f}%".format(confidence[i] * 100)
+                y = endY + 20 if endY + 20 > 10 else endY - 10
+                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                                (0, 0, 255), 2)
+                cv2.putText(frame, text, (startX, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 1)
+                return frame
+            else:
+                return frame_copy
+    else:
+        return frame_copy
         
 def annotate_distance(frame, face_boxes, pos_dict, close_objects):        
     frame_copy = frame.copy()
@@ -30,7 +33,6 @@ def annotate_distance(frame, face_boxes, pos_dict, close_objects):
             cv2.rectangle(frame, (startX, startY), (endX, endY), COLOR, 2)
             y = startY - 15 if startY - 15 > 15 else startY + 15
             # Convert cms to feet
-
             cv2.putText(frame, 'Dist. to cam: {i} cm'.format(i=round(pos_dict[i][2],4)), (startX, y),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLOR, 1)
             return frame
