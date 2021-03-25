54cdd01c3c44e3c0e1f553413620c51e7aae2540
Roy Fishermann <mszlesz@gmail.com>
2021-03-25 21:26:38 +0000

    shorter

--- src/face_detector.py
+++ src/face_detector.py
@@ -1,27 +1,29 @@
 import numpy as np
 import cv2
 
-class FaceModelLoader:	
+class FaceModelLoader:
     @staticmethod
     def load(faceProto, faceModel):
         net = cv2.dnn.readNetFromCaffe(faceProto, faceModel)
         return net
 
-class FrameProcessor:	
+
+class FrameProcessor:
     def __init__(self):
-    	self.size = 300
-    	self.scale = 1.0
-    	self.mean = (104.0, 177.0, 123.0)
-	
+        self.size = 300
+        self.scale = 1.0
+        self.mean = (104.0, 177.0, 123.0)
+
     def get_blob(self, frame):
         img = frame
         (h, w) = frame.shape[:2]
-        
+
         resized = cv2.resize(img, (self.size, self.size), cv2.INTER_AREA)
         blob = cv2.dnn.blobFromImage(resized, self.scale,
-                                    (self.size, self.size),
-                                    self.mean)
+                                     (self.size, self.size),
+                                     self.mean)
         return blob
+
 
 class FaceDetector:
     def __init__(self, faceProto, faceModel):
@@ -45,21 +47,17 @@ class FaceDetector:
                 continue
             # compute the (x, y)-coordinates of the bounding box for the
             # object
-            box = detections[0, 0, i, 3:7] * np.array([self.w, 
-                                                        self.h, 
-                                                        self.w, 
-                                                        self.h])
+            box = detections[0, 0, i, 3:7] * np.array([self.w,
+                                                       self.h,
+                                                       self.w,
+                                                       self.h])
             (startX, startY, endX, endY) = box.astype("int")
-            startX -= 15
-            startY -= 15
+            startX = max(0, startX - 15)
+            startY = max(0, startY - 15)
             endX += 15
             endY += 15
-            startX = max(0, startX)
-            startY = max(0, startY)
-            endX = max(0, endX)
-            endY = max(0, endY)
 
             confidence.append(confid_all)
-            face_boxes.append((startX, startY, endX, endY))       
-        
-        return face_boxes, confidence
+            face_boxes.append((startX, startY, endX, endY))
+
+        return tuple(face_boxes), tuple(confidence)
