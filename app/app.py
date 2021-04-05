6f8cfd314f3f44b9c2bbf828a0078db3edfa0031
Roy Fishermann <mszlesz@gmail.com>
2021-04-05 22:17:57 +0000

    types, progress bar, audio&img opt

--- app/app.py
+++ app/app.py
@@ -7,12 +7,12 @@ import tempfile
 import base64
 import subprocess as sp
 #TODO: dist calibration as type of transfer class
-#from typing import List, NamedTuple
+from typing import List, NamedTuple
 
-#try:
-#    from typing import Literal
-#except ImportError:
-#    from typing_extensions import Literal  # type: ignore
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal  # type: ignore
 
 import av
 import cv2
@@ -32,13 +32,14 @@ from src.BodyDetector import *
 from src.MaskWarning import *
 from src.FaceMaskClassifier import FaceMaskClassifier
 from src.StreamlitDesign import StreamlitDesign
+from src.BrightnessOptimizer import BrightnessOptimizer
 
 FACE_MODEL = "src/models/face_model/res10_300x300_ssd_iter_140000.caffemodel"
 FACE_PROTO = "src/models/face_model/deploy.prototxt"
 BODY_MODEL = "src/models/body_model/mobilenet.caffemodel"
 BODY_PROTO = "src/models/body_model/mobilenet.prototxt"
 MASK_MODEL = "src/models/mask_model/mnv2_mask_classifier_v3.pth"
-FACE_CONFID_THRESH = 0.3
+FACE_CONFID_THRESH = 0.5
 BODY_CONFID_THRESH = 0.5
 
 from streamlit_webrtc import (
@@ -107,49 +108,53 @@ def app_video_upload():
     legal_extensions = ["avi", "mp4"]
     uploaded_file = st.file_uploader(f"Upload a video. Mind that, the longer the video, the longer the processing time.", ["avi", "mp4"]) 
     if uploaded_file:
-        st.write("Processing the video. It may take a few minutes.")
-        tfile = tempfile.NamedTemporaryFile(delete=False)
-        tfile.write(uploaded_file.read())
-        fvs = FileVideoStream(tfile.name).start()
-        video_in = fvs.stream
-        frame_width = int(video_in.get(cv2.CAP_PROP_FRAME_WIDTH))
-        frame_height = int(video_in.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = video_in.get(cv2.CAP_PROP_FPS)
-        frame_count = int(video_in.get(cv2.CAP_PROP_FRAME_COUNT))
-        assembly = ModelAssembly()
-        
-        # See http://zulko.github.io/blog/2013/09/27/read-and-write-video-frames-in-python-using-ffmpeg/
-        command = ['ffmpeg',
-                   '-loglevel', 'error',
-                   '-y',
-                   # Input
-                   '-f', 'rawvideo',
-                   '-vcodec', 'rawvideo',
-                   '-pix_fmt', 'bgr24',
-                   '-s', str(frame_width) + 'x' + str(frame_height),
-                   '-r', str(fps),
-                   # Output
-                   '-i', '-',
-                   '-an',
-                   '-vcodec', 'h264',
-                   '-r', str(fps),
-                   '-pix_fmt', 'rgb24',
-                   "mask_guard.mp4"
-                   ]
-        pipe = sp.Popen(command, stdin=sp.PIPE)        
-        while fvs.more():
-            frame = fvs.read()
-            if frame is not None:
-                processed_frame = assembly.forwardFrame(frame)
-                pipe.stdin.write(processed_frame.tobytes())
-        pipe.stdin.close()
-        pipe.wait() 
-        bin_file = 'mask_guard.mp4' 
-        with open(bin_file, 'rb') as f:
-            data = f.read()
-        bin_str = base64.b64encode(data).decode()
-        href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download video</a>' 
-        
+        with st.spinner("Processing the video. It may take a few minutes."):
+            prog = 0
+            bar = st.progress(prog)
+            tfile = tempfile.NamedTemporaryFile(delete=False)
+            tfile.write(uploaded_file.read())
+            fvs = FileVideoStream(tfile.name).start()
+            video_in = fvs.stream
+            frame_width = int(video_in.get(cv2.CAP_PROP_FRAME_WIDTH))
+            frame_height = int(video_in.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            fps = video_in.get(cv2.CAP_PROP_FPS)
+            frame_count = int(video_in.get(cv2.CAP_PROP_FRAME_COUNT))
+            assembly = ModelAssembly()
+            
+            # See http://zulko.github.io/blog/2013/09/27/read-and-write-video-frames-in-python-using-ffmpeg/
+            command = ['ffmpeg',
+                       '-loglevel', 'error',
+                       '-y',
+                       # Input
+                       '-f', 'rawvideo',
+                       '-vcodec', 'rawvideo',
+                       '-pix_fmt', 'bgr24',
+                       '-s', str(frame_width) + 'x' + str(frame_height),
+                       '-r', str(fps),
+                       # Output
+                       '-i', '-',
+                       '-an',
+                       '-vcodec', 'h264',
+                       '-r', str(fps),
+                       '-pix_fmt', 'rgb24',
+                       "mask_guard.mp4"
+                       ]
+            pipe = sp.Popen(command, stdin=sp.PIPE)        
+            while fvs.more():
+                frame = fvs.read()
+                if frame is not None:
+                    processed_frame = assembly.forwardFrame(frame)
+                    pipe.stdin.write(processed_frame.tobytes())
+                    prog += 1
+                    bar.progress(prog / frame_count)
+            pipe.stdin.close()
+            pipe.wait() 
+            bin_file = 'mask_guard.mp4' 
+            with open(bin_file, 'rb') as f:
+                data = f.read()
+            bin_str = base64.b64encode(data).decode()
+            href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download video</a>' 
+            
         st.markdown(href, unsafe_allow_html=True)
     
 class ModelAssembly():
@@ -157,6 +162,8 @@ class ModelAssembly():
         self.face_detector = FaceDetector(FACE_PROTO, FACE_MODEL)
         self.body_detector = BodyDetector(BODY_PROTO, BODY_MODEL)
         self.face_mask_classifier = FaceMaskClassifier(MASK_MODEL)
+        self.warn = MaskWarning(cooldown=6)
+        self.optimizer = BrightnessOptimizer()
 
         self.DIST_REF = 22
         self.FOCAL = int((309 *  100) / self.DIST_REF)
@@ -165,7 +172,8 @@ class ModelAssembly():
     def __cropout(self, img, box):
         return img[box[1]:box[3], box[0]:box[2]]
 
-    def forwardFrame(self, frame):
+    def forwardFrame(self, frame, soundOn = False):
+        frame = self.optimizer.optimize(frame)
         annotater = Annotater(frame)
         face_crops = []
         face_boxes, _ = self.face_detector.detect(frame, FACE_CONFID_THRESH)
@@ -193,7 +201,9 @@ class ModelAssembly():
                     self.dist.measure(annotater.faces)
                     )
             frame = annotater.update()
-        
+            if soundOn:
+                self.warn.probe(bool("no_mask" in annotater.mask_statuses))
+       
         return frame 
  
 
@@ -202,14 +212,19 @@ def app_mask_detection():
     """ Video transforms with OpenCV """
 
     class OpenCVVideoTransformer(VideoTransformerBase):
-        #type: Literal("bla", "bla")
+        type: Literal["basic", "sound_warnings"]
         def __init__(self) -> None:
+            self.type = "basic"
             self.assembly = ModelAssembly()    
 
         def transform(self, frame: av.VideoFrame) -> av.VideoFrame:
             img = frame.to_ndarray(format="bgr24") ## PIL ?
            
-            return self.assembly.forwardFrame(img)
+            return self.assembly.forwardFrame(img, soundOn = (self.type == "sound_warnings"))
+
+    transformer_type = st.radio(
+        "Select mode", ("basic", "sound_warnings")
+    )
 
     webrtc_ctx = webrtc_streamer(
         key="opencv-filter",
@@ -219,11 +234,8 @@ def app_mask_detection():
         async_transform=True,
     )
 
-    #transform_type = st.radio(
-    #    "Select transform type", ("noop", "cartoon", "edges", "rotate")
-    #)
-    #if webrtc_ctx.video_transformer:
-    #    webrtc_ctx.video_transformer.type = transform_type
+    if webrtc_ctx.video_transformer:
+        webrtc_ctx.video_transformer.type = transformer_type
 
 if __name__ == "__main__":
     logging.basicConfig(
